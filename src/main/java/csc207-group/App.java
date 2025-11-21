52ddbd3a60892d9337808d233ca54ae297fa8ffa
Krithik Tamilselvan <krithikkeshant7489@gmail.com>
2025-11-21 16:00:13 -0500

    Initial commit of my project

---
+++ src/main/java/csc207-group/App.java
@@ -0,0 +1,13 @@
+package csc207-group;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
